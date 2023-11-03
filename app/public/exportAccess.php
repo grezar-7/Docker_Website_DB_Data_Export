58e9fde37bb15ca7c672c7e96012f27a0674153f
Daniel Jandrin <danieljandrin@yahoo.com>
2023-11-03 15:18:15 -0500

    create function in AccessExport page where it will download query results onto viewer's computer

--- app/public/exportAccess.php
+++ app/public/exportAccess.php
@@ -5,8 +5,13 @@ $pdo = new PDO('mysql:dbname=tutorial;host=mysql', 'root', 'secret', [PDO::ATTR_
 
 
 
-
-$sql =  $_POST['dbExport'];
+// When the submit button is clicked
+if (isset($_POST['exportQuery'])) {
+	$sql =  $_POST['dbExport'];
+    if($sql == null){
+    //function does nothing if there is no query
+    }
+    else{
 $connection = mysqli_connect('mysql', 'root', 'secret', 'tutorial') or die("Connection Error " . mysqli_error($connection));
 $result = mysqli_query($connection, $sql) or die("Selection Error " . mysqli_error($connection));
 
@@ -21,11 +26,9 @@ $result = mysqli_query($connection, $sql) or die("Selection Error " . mysqli_err
 mysqli_close($connection);
 echo $sql;
 echo"<br>";
-// When the submit button is clicked
-if (isset($_POST['exportQuery'])) {
 $bits = 8 * PHP_INT_SIZE;
 echo "(Info: This script is running as $bits-bit.)\r\n\r\n";
-echo"<br>";}
+echo"<br>";}}
 
 $query = $pdo->query('SHOW VARIABLES like "version"');
 
@@ -33,4 +36,8 @@ $row = $query->fetch();
 
 echo 'MySQL version:' . $row['Value'];
 ?>
+<a href="books.csv" download>
+ <img src="image_2023_11_03T18_24_20_170Z.png" alt="alt_text">
+</a>
+
 <?php echo file_get_contents("html/footer.html"); ?>
